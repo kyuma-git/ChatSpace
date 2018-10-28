ae4ee2efe657fb68ba07cbc1aca5501082781b5d
Kyuma <hokumon2@icloud.com>
2018-10-28 19:07:20 +0900

    メッセージの送信機能を実装した

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,15 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
+
   validates :name, presence: :true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像だけ投稿されてます'
+    else
+      'まだメッセージなし'
+    end
+  end
 end
